cda203e24e65587c8713cf38c71b37c0eea67344
Luc MULLER <l.muller@ameos.local>
2022-04-06 10:18:53 +0200

    [FEATURE]Compatibily with TYPO3 11 [FEATURE]Raise version to 1.0.8

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email'     => 'typo3dev@ameos.com',
     'author_company'   => 'Ameos',
     'state'            => 'stable',
-    'version'          => '1.0.7',
+    'version'          => '1.0.8',
     'constraints'      => [
         'depends' => [
-            'typo3' => '8.7.0-10.4.99',
+            'typo3' => '8.7.0-11.5.99',
             'php'   => '7.0.0-7.3.99'
         ],
         'conflicts' => [],
