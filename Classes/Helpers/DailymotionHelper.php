a219cb90a69a3a4d7106dbce8cc91fafde242bb0
Stéphane Fillion <s.fillion@ameos.com>
2024-01-23 09:41:39 +0100

    TYPO3 12 Compatibility /2

--- Classes/Helpers/DailymotionHelper.php
+++ Classes/Helpers/DailymotionHelper.php
@@ -1,25 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Ameos\AmeosDailymotion\Helpers;
 
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\Folder;
+use TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\AbstractOEmbedHelper;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
-class DailymotionHelper extends \TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\AbstractOEmbedHelper
+class DailymotionHelper extends AbstractOEmbedHelper
 {
     /**
      * Get public url
