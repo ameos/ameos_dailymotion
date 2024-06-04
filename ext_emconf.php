ed05a60f330e34d430a614c60273d3a3d2ac3064
Luc MULLER <l.muller@ameos.local>
2024-06-04 17:01:41 +0200

    [RELEASE] Version 2.1.0 [FEATURE] Add TYPO3 13 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF['ameos_dailymotion'] = [
     'author_email'     => 'typo3dev@ameos.com',
     'author_company'   => 'Ameos',
     'state'            => 'stable',
-    'version'          => '2.0.0',
+    'version'          => '2.1.0',
     'constraints'      => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.99.99',
             'php'   => '8.0.0-8.3.99'
         ],
         'conflicts' => [],
